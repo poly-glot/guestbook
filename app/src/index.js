3d8a2a936bbc5439ef1ed3417a525acaaa6bb420
Junaid Ahmed <junaidahmed@teamitg.com>
2023-10-24 20:42:10 +0100

    upgraded to develop v2

--- app/src/index.js
+++ app/src/index.js
@@ -29,7 +29,7 @@ db.mongoose
 
 // simple route
 app.get("/", (req, res) => {
-    res.json({ message: "Welcome to Guestbook application - develop" });
+    res.json({ message: "Welcome to Guestbook application - develop v2" });
 });
 
 require("./routes/post.routes")(app);
